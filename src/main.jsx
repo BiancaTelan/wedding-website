2d61bad489790b382e315f164330e1efc5c2b708
Dionysus <158821418+Cybeles-Eos@users.noreply.github.com>
2025-08-15 10:10:45 +0800

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -5,4 +5,6 @@ import App from "./App.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+<App />
+);
